3ba783f44abae16b717b24f6a1af151f94fb1949
Narumi <leticiafujiharu@hotmail.com>
2025-07-25 23:16:27 -0300

    Adicionei novas funcinalidades

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,36 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (resquest, response) => {
-    console.log('Listagem de usuarios');
+const users = [
+    'Diego',   //0
+    'Leticia', //1
+    'Eron',    //2
+    'Daniel'   //3
+];
 
-    response.send('Leticia');
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = users.filter(user => user.includes(search));
+
+return response.json(filteredUsers);
 });
 
-app.listen(3333);
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br'
+    };
+
+    return response.json(user);
+});
+
+app.listen(3334);
