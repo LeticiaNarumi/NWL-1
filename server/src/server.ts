8aa681281da4539dbc676ddcd10a9fa2f16bbfd0
Narumi <leticiafujiharu@hotmail.com>
2025-07-28 21:37:57 -0300

    updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-const users = [
-    'Diego',   //0
-    'Leticia', //1
-    'Eron',    //2
-    'Daniel'   //3
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = users.filter(user => user.includes(search));
-
-return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) => {
-    const user = {
-        name: 'Diego',
-        email: 'diego@rocketseat.com.br'
-    };
-
-    return response.json(user);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3334);
